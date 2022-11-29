f9bc837e0e804fd89c2726d4d916c1020413081a
cldclh <119423919+cldclh@users.noreply.github.com>
2022-11-29 22:25:26 +0800

    Update server-config.js

--- config/server-config.js
+++ config/server-config.js
@@ -1,4 +1,33 @@
-// 此时间为每天的早上8点，*为匹配任意一个
-// 这里的时间是中国时间 秒 分 时 日 月 年
-const cornTime = '0 0 8 * * *'
-export default cornTime
+expoort const config={
+  //公众号配置
+  //公众号appId
+  appId: "
+wx6764e0d317b52703"
+  //公众号appSecret
+  appSecret: "2ccb340720852f2f93bc4f3531889472"
+  //模板消息id
+  templateId: "3tYRWpWrUjibTDVUzkDAKOWNwUzQw4G-7I5JuhrKEr4"
+  user: [Iae5244],
+  //信息配置
+  //所在省份  
+  province: "山西"
+//所在城市
+city: "太原"
+birthdays: [
+  {"name": "亲爱的李晨浩宝宝"，"year":"2000","date": "09-16","type":'new'}
+  ],
+  //在一起的日子
+  loveDate: "2022-11-15",
+}
+export const config = {
+  //公众号配置
+   //公众号配置appId
+  appId:"wx6764e0d317b52703"
+   //公众号appSecret
+  appSecret: "2ccb340720852f2f93bc4f3531889472",
+  //模板消息id
+  templateId: "3tYRWpWrUjibTDVUzkDAKOWNwUzQw4G-7I5JuhrKEr4",
+  user: [Iae5244],
+    
+    //({note_en.DATA})
+    //({note_ch.DATA})
